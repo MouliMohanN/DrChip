fa09b29e042a0890e955b64a707698ea2f990c7f
MouliMohanN <moulimohann@gmail.com>
2016-05-29 09:25:36 +0530

    added material calendar view

---
+++ app/src/main/java/com/drchip/android/views/custom/materialcalederview/DecoratorResult.java
@@ -0,0 +1,11 @@
+package com.drchip.android.views.custom.materialcalederview;
+
+class DecoratorResult {
+    public final DayViewDecorator decorator;
+    public final DayViewFacade result;
+
+    DecoratorResult(DayViewDecorator decorator, DayViewFacade result) {
+        this.decorator = decorator;
+        this.result = result;
+    }
+}
